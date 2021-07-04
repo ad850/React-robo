f8cbb95058c154d0e7f3df8bac77dbf690c46987
Aditya Sharma <83112843+ad850@users.noreply.github.com>
2021-07-04 12:23:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import React from 'react';
 import Header from './Header';
 import Search from './Search'
 import Cardlist from './Cardlist';
+import List from './List';
 
-const App = () => {
-
-
-    return (
-        <>
-            <Header />
-            <Search />
-            <Cardlist />
+class App extends React.Component {
 
 
-        </>
-    )
+    constructor() {
 
+        super();
+        this.state = {
+
+            List: List,
+            searchfield: ''
+
+        }
+
+    }
+
+
+    onchange = (event) => {
+
+        this.setState({ searchfield: event.target.value });
+
+    }
+    render() {
+
+        const filteredlist = this.state.List.filter(list => {
+
+
+            return list.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+     return (
+            <>
+                <Header />
+                <Search searchchange={this.onchange} />
+                <Cardlist List={filteredlist} />
+
+
+            </>
+        )
+    }
 }
+
 export default App;
